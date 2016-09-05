01738da06310118ede9060158fe83f2a68c4125d
YuanChao <yuanchaocs@163.com>
2016-09-05 14:37:40 +0800

    修改了一下注释MainActivity

--- app/src/main/java/com/feicuiedu/newsdemo/MainActivity.java
+++ app/src/main/java/com/feicuiedu/newsdemo/MainActivity.java
@@ -3,6 +3,9 @@ package com.feicuiedu.newsdemo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+/**
+ * 这是当前应用入口页面
+ */
 public class MainActivity extends AppCompatActivity {
 
     @Override
