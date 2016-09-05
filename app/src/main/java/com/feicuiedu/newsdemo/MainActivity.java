bc85d49c34838d35d3582184e830c34b22a087fa
YuanChao <yuanchaocs@163.com>
2016-09-05 17:47:48 +0800

    加入ButterKnife

--- app/src/main/java/com/feicuiedu/newsdemo/MainActivity.java
+++ app/src/main/java/com/feicuiedu/newsdemo/MainActivity.java
@@ -1,17 +1,47 @@
 package com.feicuiedu.newsdemo;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.Toast;
+
+import butterknife.BindString;
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import butterknife.Unbinder;
 
 /**
  * 这是当前应用入口页面
  */
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity{
 
-    // aaaaaaa
+    @BindString(R.string.app_name) String appName;
+
+    private Unbinder unbinder;
+
+
+    @BindView(R.id.button) Button button;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        unbinder = ButterKnife.bind(this);
+    }
+
+    @Override protected void onDestroy() {
+        super.onDestroy();
+        unbinder.unbind();
+    }
+
+    @OnClick(R.id.button)
+    public void clickButton() {
+        Toast.makeText(MainActivity.this, "button", Toast.LENGTH_SHORT).show();
+    }
+
+    @OnClick(R.id.textview)
+    public void clickTextView() {
+        Toast.makeText(MainActivity.this, appName, Toast.LENGTH_SHORT).show();
     }
 }
